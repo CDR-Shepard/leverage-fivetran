6750c0839e435d44ccd15f00e87a2341bcb37017
CDR-Shepard <56300999+CDR-Shepard@users.noreply.github.com>
2024-08-03 15:52:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
  function connect(crm) {
     if (crm === 'salesforce') {
-        window.open('https://fivetran.com/connect-card/setup?redirect_uri=https://dmforce.com/success&auth=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7ImxvZ2luIjp0cnVlLCJ1c2VyIjoiY2hlbWljYWxseV9wcm9mbGlnYWN5IiwiYWNjb3VudCI6InN3YXBwaW5nX3R1Y2tlZCIsImdyb3VwIjoidHJhbnNmZXJyZWRfaW5kZWxpYmxlIiwiY29ubmVjdG9yIjoiaW5oYWJpdF9wcm92b2xvbmUiLCJtZXRob2QiOiJQYmZDYXJkIn0sImlhdCI6MTcyMjYzMDY0MH0.QfSO7uVHxhGpSP-IZKlEw4o1u0DaNo8wKNa3x0d8wHs&hide_setup_guide=true', '_blank');
+        window.open('https://fivetran.com/connect-card/setup?redirect_uri=https://dmforce.com/success&auth=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7ImxvZ2luIjp0cnVlLCJ1c2VyIjoiY2hlbWljYWxseV9wcm9mbGlnYWN5IiwiYWNjb3VudCI6InN3YXBwaW5nX3R1Y2tlZCIsImdyb3VwIjoidHJhbnNmZXJyZWRfaW5kZWxpYmxlIiwiY29ubmVjdG9yIjoidG90YWxseV9zZW50aW1lbnRhbCIsIm1ldGhvZCI6IlBiZkNhcmQifSwiaWF0IjoxNzIyNzI1NTI2fQ.QKHfxE7EKOzXfFScbO7kj9jEppF7b6UMc_caJiHpTmY&hide_setup_guide=true', '_blank');
     } else {
         console.log('Connect function for ' + crm + ' is not implemented yet.');
     }
 }
-// v 0.13 08-02
+// v 0.14 08-03
